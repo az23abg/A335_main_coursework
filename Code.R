3d3feba4c60d23e42a07176c07fc29ca693e0bba
Syed Muhammad Mubbashir Hussnain Shah <ss23anm@herts.ac.uk>
2024-11-21 22:50:40 +0000

    Check Mean and Median of the Temperature and CO2.

--- Code.R
+++ Code.R
@@ -13,3 +13,19 @@ summary(carbon_segment)
 
 # View the full dataset in RStudio (opens in a new tab)
 View(carbon_segment)
+
+# Calculate mean and median for Temp and CO2
+temp_mean <- mean(carbon_segment$Temp, na.rm = TRUE)
+print(temp_mean)
+temp_median <- median(carbon_segment$Temp, na.rm = TRUE)
+print(temp_median)
+co2_mean <- mean(carbon_segment$CO2, na.rm = TRUE)
+co2_median <- median(carbon_segment$CO2, na.rm = TRUE)
+
+# Display results
+cat("Temperature (°C):\n")
+cat("Mean:", temp_mean, "\nMedian:", temp_median, "\n\n")
+
+cat("CO₂ Concentrations (ppm):\n")
+cat("Mean:", co2_mean, "\nMedian:", co2_median, "\n")
+
